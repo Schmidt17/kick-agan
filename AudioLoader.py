6603a97f3517b61b1823add80232530fda897768
ah <andreas.hofacker@posteo.de>
2020-01-16 23:45:03 +0100

    Initial commit

---
+++ AudioLoader.py
@@ -0,0 +1,44 @@
+import aifc
+import numpy as np
+
+def load_aif(fpath, downsample_by=1):
+	with aifc.open(fpath) as audio_file:
+		N_frames = audio_file.getnframes()
+		audio_frames = audio_file.readframes(N_frames)
+	audio_arr = load24bitbufferas(audio_frames, np.float32)
+	# separate the two channels (LRLRLRLRLR....)
+	audio_arr = audio_arr.reshape((-1, 2))
+	# and mix them together
+	mono_mix = np.sum(audio_arr, axis=-1)
+	# normalize to [0., 1.]
+	mono_mix -= np.min(mono_mix)
+	mono_mix /= np.max(mono_mix)
+	mono_mix = mono_mix[::downsample_by]  # downsampling
+
+	return mono_mix
+
+def save_aif(data, fpath, ds=1):
+	with aifc.open(fpath, mode="wb") as f:
+		f.setnchannels(1)
+		f.setsampwidth(2)
+		f.setframerate(96000 // ds)
+		m = np.min(data)
+		data -= m
+		data = data.astype('>i2')
+		data += m.astype('>i2')
+		f.writeframes(data.tostring())
+
+
+def load24bitbufferas(buff, dtype=np.float32, endianess='big'):
+	assert endianess in ['big', 'small'], f"Unrecognized endianess specifier '{endianess}'"
+
+	# load the 24-bit buffer string into a 32-bit numpy array:
+	# load bytewise (in chunks of int8); group 3 bytes (24 bit) together as one frame
+	raw_audio_bytes = np.frombuffer(buff, 'b').reshape((-1, 3))
+	if endianess == 'small':
+		raw_audio_bytes = raw_audio_bytes[:, ::-1]  # reverse byte order
+	audio_arr = np.zeros((raw_audio_bytes.shape[0], 4), dtype='b')  # template array for 32-bit groups
+	audio_arr[:, :-1] = raw_audio_bytes  # leave the last byte (least significant byte - file is big-endian) at zero (to correctly copy the sign bit)
+	audio_arr = audio_arr.flatten().view('>i4') >> 8  # flatten the bytes out and return a big-endian signed 32-bit view of the array, shift the bits back to right significance, leaving sign correct, since python bitshift does not shift the sign bit
+
+	return audio_arr.astype(dtype)  # cast the array to the desired type
---
+++ aifgan.py
@@ -0,0 +1,152 @@
+import os
+import numpy as np
+import AudioLoader
+import matplotlib.pyplot as plt
+import tensorflow as tf
+tf.random.set_seed(1000)
+
+def discriminator(seq_len):
+	""" Discriminator net with binary output: Real (1) or fake (0) """
+	model = tf.keras.Sequential([
+			tf.keras.layers.Conv1D(filters=64, kernel_size=5, 
+				strides=2, padding='same', activation='relu', input_shape=(seq_len, 1)),
+			# shape=(None, seq_len*2, 64)
+			tf.keras.layers.Dropout(0.3),
+			tf.keras.layers.Conv1D(filters=128, kernel_size=5, 
+				strides=2, padding='same', activation='relu'),
+			# shape=(None, seq_len*4, 128)
+			tf.keras.layers.Dropout(0.3),
+			tf.keras.layers.Flatten(),
+			# shape=(None, seq_len*4*128)
+			tf.keras.layers.Dense(16, activation="relu"),
+			# shape=(None, 16)
+			tf.keras.layers.Dense(1, activation="sigmoid")
+			# shape=(None, 1)
+		])
+	return model
+
+def generator(seq_len):
+	""" Generator net which outputs an audio sample of length `seq_len` from a seed of length 100"""
+	assert seq_len % 4 == 0, "Sequence length must be divisible by 4"
+	dense_dim = (seq_len // 4)
+
+	model = tf.keras.Sequential([
+			tf.keras.layers.Dense(dense_dim * 64, use_bias=False, input_shape=(100,), activation='linear'),
+			tf.keras.layers.LeakyReLU(),
+			# shape=(None, seq_len/4 * 64)
+
+			tf.keras.layers.Reshape((1, dense_dim, 64)),
+			# shape=(None, 1, seq_len/4, 64)
+
+			tf.keras.layers.Conv2DTranspose(filters=128, kernel_size=(1, 5),  use_bias=False,
+				strides=(1, 1), padding='same', activation='relu'),
+			# shape=(None, 1, seq_len/4, 128)
+
+			tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=(1, 5),  use_bias=False,
+				strides=(1, 1), padding='same', activation='linear'),
+			tf.keras.layers.BatchNormalization(),
+			tf.keras.layers.LeakyReLU(),
+			# shape=(None, 1, seq_len/4, 64)
+
+			tf.keras.layers.Conv2DTranspose(filters=32, kernel_size=(1, 5),  use_bias=False,
+				strides=(1, 2), padding='same', activation='relu'),
+			# shape=(None, 1, seq_len/2, 32)
+
+			tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=(1, 5),  use_bias=False,
+				strides=(1, 2), padding='same', activation='tanh'),
+			# shape=(None, 1, seq_len, 1)
+
+			tf.keras.layers.Reshape((seq_len, 1))
+			# shape=(None, seq_len, 1)
+		])
+	return model
+
+# load all kick samples from Analog folder
+downs = 20
+aif_paths = [os.path.join("Analog", f) for f in os.listdir("Analog") if f.endswith(".aif")]
+audio_data = []
+for path in aif_paths:
+	audio_data.append(AudioLoader.load_aif(path, downsample_by=downs))
+
+max_len = max([arr.shape[0] for arr in audio_data])
+if max_len % 4 != 0:  # make sure the desired length is a multiple of 4, to suit generator design
+	max_len += 4 - (max_len % 4)
+# pad all the files with their last value to the max_len
+audio_data = np.array([np.pad(data, (0, max_len-data.shape[0]), mode='edge') for data in audio_data])
+
+ce = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def disc_loss(real_output, fake_output):
+	real_loss = ce(tf.ones_like(real_output), real_output)
+	fake_loss = ce(tf.zeros_like(fake_output), fake_output)
+	return real_loss + fake_loss
+
+def generator_loss(fake_output):
+	return ce(tf.ones_like(fake_output), fake_output)
+
+lr = 0.5e-4  		# learning rate
+num_epochs = 50
+batch_size = 1
+noise_len = 100		# length of noise seed for generator
+num_gens = 16
+seed = tf.random.normal((num_gens, noise_len))
+
+disc_opt = tf.keras.optimizers.Adam(lr)
+gen_opt = tf.keras.optimizers.Adam(2.*lr)
+
+train_samples = audio_data.reshape((audio_data.shape[0] // batch_size, batch_size, max_len, 1))
+
+@tf.function
+def train_step(train_audio):
+	local_noise = tf.random.normal((batch_size, noise_len))
+
+	with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+		generated_samples = G(local_noise, training=True)
+
+		real_output = D(train_audio, training=True)
+		fake_output = D(generated_samples, training=True)
+
+		gen_loss = generator_loss(fake_output)
+		d_loss = disc_loss(real_output, fake_output)
+
+	gen_gradients = gen_tape.gradient(gen_loss, G.trainable_variables)
+	disc_gradients = disc_tape.gradient(d_loss, D.trainable_variables)
+
+	gen_opt.apply_gradients(zip(gen_gradients, G.trainable_variables))
+	disc_opt.apply_gradients(zip(disc_gradients, D.trainable_variables))
+
+D = discriminator(max_len)
+G = generator(max_len)
+
+def train(data, epochs):
+	for epoch in range(epochs):
+		print(f"Start epoch {epoch}")
+		for sample_batch in data:
+			train_step(sample_batch)
+
+		# generate and plot a sample for progress monitoring (always the same seed for consistency)
+		gen = G(seed, training=False)
+		update_plot(gen[0, :, 0])
+
+fig, ax = plt.subplots(1, 1)
+rdata = np.random.normal(size=max_len)  # some random data for initializing the plot
+pls = ax.plot(audio_data[5], color='red')
+pl = ax.plot(rdata, color='blue')[0]
+plt.show(block=False)
+
+def update_plot(data):
+	print(data.shape)
+	pl.set_ydata(data)
+	fig.canvas.draw()
+	plt.pause(0.01)
+
+train(train_samples, num_epochs)  # train the thing
+
+G.save("generator.h5")
+
+# save example as audio file (kinda hacky conversion from float[0., 1.] to signed int16)
+gen = G(seed, training=False)
+AudioLoader.save_aif((gen[0, :, 0].numpy()-0.5) * 2**15, "test.aif", downs)
+
+# show again so the window doesn't close and we can LOok At IT!
+plt.show(block=True)
